106ba0b2b3347429a2dd801ebd60809f4fb0c7ee
lkzm <lkzmkz@gmail.com>
2019-08-10 23:48:37 +0300

    some models

--- music/models.py
+++ music/models.py
@@ -1,20 +1,26 @@
 from django.db import models
+from music import gvars
 
 # Create your models here.
 
 
 class User(models.Model):
-    name = models.CharField(max_length = 69)
+    name = models.CharField(max_length = gvars.names)
     
 
 class Collection (models.Model):
     user = models.ForeignKey('User', on_delete = models.CASCADE)
-    name = models.CharField(max_length = 69)
-    description = models.CharField (max_length = 255, default = 'none')
+    name = models.CharField(max_length = gvars.names)
+    description = models.CharField (max_length = gvars.text, default = 'none')
 
 
 def path_fix(album, artist)
 class Track (models.Model):
-    album = models.CharField(max_length = 69)
-    artist = models.CharField(max_length = 69)
-    file = model.FileField(upload.to='artist/al
+    title = models.CharField(max_length = gvars.names)
+    album = models.CharField(max_length = gvars.names)
+    artist = models.CharField(max_length = gvars.names)
+    file = models.FileField()
+    track_number = models.IntegerField(default=0)
+
+    
+
