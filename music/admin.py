84276462f441f1668258e2f455dc6ec09b46509c
lkzm <lkzmkz@gmail.com>
2019-08-10 23:56:37 +0300

    add to admin

--- music/admin.py
+++ music/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from music import models
+admin.site.register(models.Collection)
+admin.site.register(models.Track)
+admin.site.register(models.User)
 # Register your models here.
