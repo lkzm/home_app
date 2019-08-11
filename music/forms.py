a633282f8b11c410930129b92ec33f36144d2741
lkzm <lkzmkz@gmail.com>
2019-08-11 21:56:34 +0300

    basic views for track

---
+++ music/forms.py
@@ -0,0 +1,19 @@
+from music import models
+from django import forms
+from music import gvars
+
+class UploadFileForm (forms.Form):
+    file = forms.FileField(allow_empty_file=False)
+
+
+class MetaDataForm (forms.Form):
+    title = forms.CharField(max_length=gvars.names)
+    album = forms.CharField(max_length=gvars.names)
+    artist = forms.CharField(max_length=gvars.names)
+    track_number = forms.IntegerField()
+
+class CreateCollectionForm (forms.Form):
+    name = forms.CharField(max_length=gvars.names)
+    description = forms.CharField(max_length=gvars.text)
+
+
